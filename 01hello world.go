e301889c1f0c36ad0eaa55dbcff0dc00534335b9
TeddyDai <58017835+TeddyDai@users.noreply.github.com>
2019-12-09 15:23:58 +0800

    Update 01hello world.go
    the world say hello to you

--- 01hello world.go
+++ 01hello world.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println("hello Boy")
+	fmt.Println("Have a nice day")
+	fmt.Println("and enjoy your life")
+	fmt.Println("be yourself and find inner peace in yourself")
 }
